23f3f80adbf1e9272bfc0ac849548e1157a3ece2
Bill Neff <BNeff@synacor.com>
2018-03-28 09:57:32 -0400

    export types from main package so consumers can use the enums it provides

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ export { LocalBatchLink } from './src/apollo/local-batch-link';
 export { ZimbraErrorLink } from './src/apollo/zimbra-error-link';
 export { ZimbraInMemoryCache } from './src/apollo/zimbra-in-memory-cache';
 export { batchJsonRequest, jsonRequest } from './src/request';
+import * as types from './types';
+export { types };
