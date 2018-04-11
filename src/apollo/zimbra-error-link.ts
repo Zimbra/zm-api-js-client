d2d0b9a21f9b7df95cbe09a4abff5995970d658c
LaveG <lave.gupta@pnq-lgupta.local>
2018-04-12 00:04:07 +0530

    PREAPPS-456: remove some line of code which was not passing errors to component as response

--- src/apollo/zimbra-error-link.ts
+++ src/apollo/zimbra-error-link.ts
@@ -1,22 +1,12 @@
 import { ApolloLink } from 'apollo-link';
 import { onError } from 'apollo-link-error';
 
-const PARTIAL_SUPPORT = ['getContactFrequency', 'relatedContacts'];
-
-const ZimbraErrorLink: ApolloLink = onError(
-	({ operation, response, graphQLErrors }) => {
-		if (
-			graphQLErrors &&
-			PARTIAL_SUPPORT.indexOf(operation.operationName) !== -1
-		) {
-			graphQLErrors.map(({ message, path }) =>
-				console.log(`[Unsupported API]: ${message}, Path: ${path}`)
-			);
-			if (response) {
-				response.errors = undefined;
-			}
-		}
+const ZimbraErrorLink: ApolloLink = onError(({ graphQLErrors }) => {
+	if (graphQLErrors) {
+		graphQLErrors.map(({ message, path }) =>
+			console.log(`[Unsupported API]: ${message}, Path: ${path}`)
+		);
 	}
-);
+});
 
 export { ZimbraErrorLink };
