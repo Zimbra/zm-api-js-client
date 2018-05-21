859097bdfb0067e4c9d573b073fa4304df821b80
Peter Lenahan <peter.lenahan@synacor.com>
2018-05-21 19:57:23 -0400

    PREAPPS-381: Print all graphql errors to the console by default.

--- src/apollo/zimbra-error-link.ts
+++ src/apollo/zimbra-error-link.ts
@@ -3,8 +3,8 @@ import { onError } from 'apollo-link-error';
 
 const ZimbraErrorLink: ApolloLink = onError(({ graphQLErrors }) => {
 	if (graphQLErrors) {
-		graphQLErrors.map(({ message, path }) =>
-			console.log(`[Unsupported API]: ${message}, Path: ${path}`)
+		graphQLErrors.map(({ message, ...rest }) =>
+			console.error(`[GraphQL]: ${message}:`, rest)
 		);
 	}
 });
