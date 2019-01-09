333e3f16553723df24af3bfa90e2640710127307
Saket Patel <saket.patel@synacor.com>
2019-01-09 17:51:36 +0530

    12.0.0-beta.2

--- src/schema/generated-schema-types.ts
+++ src/schema/generated-schema-types.ts
@@ -98,17 +98,8 @@ export interface CalendarItemMessageInput {
 
 export interface CalendarItemInviteInput {
 	components: (CalendarItemInviteComponentInput | null)[];
-	replies?: InviteReplies[] | null;
 }
 
-export interface InviteReplies {
-	  reply?: CalendarItemReply[] | null;
-}
-	
-export interface CalendarItemReply {
-	  participationStatus?: ParticipationStatus | null;
-	  attendee?: string | null;
-	}
 export interface CalendarItemInviteComponentInput {
 	name: string;
 
