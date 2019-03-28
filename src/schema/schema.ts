ad0e38b7bcb702c38a4fef2df1795d8f676410ea
Prashant Sharma <itsmeprashant58@gmail.com>
2019-03-28 16:35:18 +0530

    PREAPPS-2176-2 PREAPPS-2176 API to fetch available locals

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -102,6 +102,7 @@ export function createZimbraSchema(
 					client.getFolder(variables as GetFolderOptions),
 				getAppointments: (_: any, variables) =>
 					client.search(variables as SearchOptions),
+				getAvailableLocales: (_: any) => client.getAvailableLocales(),
 				getMailboxMetadata: (_: any, variables) =>
 					client.getMailboxMetadata(variables as GetMailboxMetadataOptions),
 				getMessage: (_, variables) =>
