786752948283545608c54a3aec97fc416e9b0dd8
Madhav Dhuppe <madhav.dhuppe@synacor.com>
2019-05-07 13:03:30 +0530

    PREAPPS-2261: Code update.

--- src/schema/session-handler.ts
+++ src/schema/session-handler.ts
@@ -3,15 +3,15 @@ import get from 'lodash/get';
 import { ZimbraInMemoryCache } from '../apollo/zimbra-in-memory-cache';
 import { ZimbraSessionOptions } from './types';
 
-const SESSIONID_GQL_FRAGMENT = gql`
-	fragment sessionId on Session {
+const SESSION_GQL_FRAGMENT = gql`
+	fragment session on Session {
 		id
 	}
 `;
 
 export class SessionHandler {
 	private cache: ZimbraInMemoryCache;
-	private sessionIdKey: string = 'sessionId';
+	private cacheKeyForSession: string = 'Session';
 
 	constructor(options: ZimbraSessionOptions) {
 		this.cache = options.cache;
@@ -19,16 +19,16 @@ export class SessionHandler {
 
 	public readSessionId = () => {
 		const sessionIdFragment: any = this.cache.readFragment({
-			id: this.sessionIdKey,
-			fragment: SESSIONID_GQL_FRAGMENT
+			id: this.cacheKeyForSession,
+			fragment: SESSION_GQL_FRAGMENT
 		});
 		return get(sessionIdFragment, 'id') || '1';
 	};
 
 	public writeSessionId = (sessionId: string) => {
 		this.cache.writeFragment({
-			id: this.sessionIdKey,
-			fragment: SESSIONID_GQL_FRAGMENT,
+			id: this.cacheKeyForSession,
+			fragment: SESSION_GQL_FRAGMENT,
 			data: {
 				id: sessionId,
 				__typename: 'Session'
