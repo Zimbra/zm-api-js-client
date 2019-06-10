3986648ee2610a748afac2fc5d8cf224b435e42e
Savita Pandit <savita.pandit@PNQ-SPANDIT.local>
2019-06-10 08:53:00 +0530

    PREAPPS-2454 completed api implementation

--- src/batch-client/index.ts
+++ src/batch-client/index.ts
@@ -489,6 +489,18 @@ export class ZimbraBatchClient {
 			}
 		}).then(res => normalize(FreeBusy)(res.usr));
 
+	public generateScratchCodes = (username: String) =>
+		this.jsonRequest({
+			name: 'GenerateScratchCodes',
+			namespace: Namespace.Account,
+			body: {
+				account: {
+					by: 'name',
+					_content: username
+				}
+			}
+		});
+
 	public getAttachmentUrl = (attachment: any) =>
 		getAttachmentUrl(attachment, {
 			origin: this.origin,
@@ -614,9 +626,22 @@ export class ZimbraBatchClient {
 			body: denormalize(GetRightsRequest)(options)
 		}).then(normalize(AccountRights));
 
+	public getScratchCodes = (username: String) =>
+		this.jsonRequest({
+			name: 'GetScratchCodes',
+			namespace: Namespace.Account,
+			body: {
+				account: {
+					by: 'name',
+					_content: username
+				}
+			}
+		});
+
 	public getSearchFolder = () =>
 		this.jsonRequest({
-			name: 'GetSearchFolder'
+			name: 'GetSearchFolder',
+			namespace: Namespace.Account
 		}).then((res: any) =>
 			res.search ? { folders: normalize(Folder)(res.search) } : {}
 		);
