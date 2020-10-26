ae9ddecbe01e1a2266278f48cbd949c7f88543fc
Aumin Patel <auminpatel007@gmail.com>
2020-10-26 10:44:53 +0530

    PREAPPS-5238 : adding name property to itemAction

--- src/schema/generated-schema-types.ts
+++ src/schema/generated-schema-types.ts
@@ -1,5 +1,4 @@
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -2624,6 +2623,7 @@ export type MutationItemActionArgs = {
   ids?: Maybe<Array<Maybe<Scalars['ID']>>>;
   folderId?: Maybe<Scalars['ID']>;
   op: Scalars['String'];
+  name?: Maybe<Scalars['String']>;
 };
 
 
