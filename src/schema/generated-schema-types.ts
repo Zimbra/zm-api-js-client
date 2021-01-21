e6764131266d5e7f42b1d21a393f44e73c93be11
Sachin Patel <sachinpatel1488@gmail.com>
2021-01-21 17:54:19 +0530

    PREAPPS-5623: Added local var in getFolderOptions

--- src/schema/generated-schema-types.ts
+++ src/schema/generated-schema-types.ts
@@ -3361,6 +3361,7 @@ export type QueryGetFolderArgs = {
   depth?: Maybe<Scalars['Int']>;
   traverseMountpoints?: Maybe<Scalars['Boolean']>;
   folder?: Maybe<GetFolderFolderInput>;
+  local?: Maybe<Scalars['Boolean']>;
 };
 
 
