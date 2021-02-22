f6c5d00ec9976ba3bf90e8610b37fd8854645d8a
Aumin Patel <auminpatel007@gmail.com>
2021-02-22 16:40:50 +0530

    PREAPPS-5503: adding the unreadDescendent to check if any subfolder or the folder itself has a unread mails in the hireracy Function setUnreadDescendentFlag will check only if the folder View is null or Message , null because the follders that are through imap or pop hasUnreadDescendent will set the flag unreadDescendent if a folders subfolder or itself has a unread count

--- src/batch-client/constants.ts
+++ src/batch-client/constants.ts
@@ -1,2 +1,17 @@
 // Zimbra Prefs which needs to be casted, from string type to array type & vice versa
 export const CASTING_PREFS = ['zimbraPrefMailTrustedSenderList'];
+
+export const USER_FOLDER_IDS = {
+	ROOT: 1,
+	INBOX: 2,
+	TRASH: 3,
+	JUNK: 4,
+	SENT: 5,
+	DRAFTS: 6,
+	CONTACTS: 7,
+	CALENDAR: 10,
+	EMAILED_CONTACTS: 13,
+	CHAT: 14,
+	TASKS: 15,
+	BRIEFCASE: 16
+};
