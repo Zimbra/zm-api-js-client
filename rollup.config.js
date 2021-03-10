c52078a44a1412fd1c316b1a4df65544dd57430f
Sachin Patel <sachinpatel1488@gmail.com>
2021-03-10 19:46:49 +0530

    PREAPPS-5617: Removed cross-fetch and accepting request api from params in case it presents

--- rollup.config.js
+++ rollup.config.js
@@ -15,8 +15,8 @@ let external = FORMAT==='es' ?
 	Object.keys(pkg.dependencies)
 		.concat(
 			['castArray', 'get','isError', 'isObject', 'mapValues', 'reduce', 'omitBy', 'uniqBy', 'concat', 'uniqBy', 'differenceBy', 'forEach'].map(v => 'lodash/'+v),
-			['graphql', '@graphql-tools/schema', 'cross-fetch']) :
-	['@graphql-tools/schema', 'cross-fetch'];
+			['graphql', '@graphql-tools/schema']) :
+	['@graphql-tools/schema'];
 
 
 export default {
