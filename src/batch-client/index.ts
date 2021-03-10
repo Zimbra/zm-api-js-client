05df45f038d5a7e8374d4a5be6a657381da3e08a
Sachin Patel <sachinpatel1488@gmail.com>
2021-03-10 19:46:49 +0530

    PREAPPS-5617: Removed cross-fetch and accepting request api from params in case it presents

--- src/batch-client/index.ts
+++ src/batch-client/index.ts
@@ -49,7 +49,8 @@ import {
 	batchJsonRequest,
 	DEFAULT_HOSTNAME,
 	DEFAULT_SOAP_PATHNAME,
-	jsonRequest
+	jsonRequest,
+	setRequestAPI
 } from '../request';
 import {
 	JsonRequestOptions,
@@ -312,6 +313,10 @@ export class ZimbraBatchClient {
 			batch: false,
 			cache: false
 		});
+
+		if(options.requestAPI){
+			setRequestAPI(options.requestAPI);
+		}
 	}
 
 	public accountInfo = () =>
