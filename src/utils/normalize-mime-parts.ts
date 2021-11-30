6c1f7a9503e49cb136289a5b415bc0e2f07eefe0
sakshirawat0247 <sakshi.rawat@telusinternational.com>
2021-11-30 12:04:07 +0530

    ZBUG-2248 Attachment (.p7m) present in an email NOT visible from Webmail Modern UI - made changes in normalize-mime-parts

--- src/utils/normalize-mime-parts.ts
+++ src/utils/normalize-mime-parts.ts
@@ -31,7 +31,6 @@ function reduceMimeParts(
 
 	return accumulator;
 }
-
 export function getAttachmentUrl(
 	attachment: { [key: string]: any },
 	{ origin = '', jwtToken }: { jwtToken?: string; origin?: string }
@@ -145,7 +144,7 @@ export function normalizeMimeParts(
 			if (!isBody && type.split('/')[0] !== 'multipart') {
 				let mode = disposition === 'inline' ? 'inlineAttachments' : 'attachments';
 
-				part.contentType !== 'application/pkcs7-mime' &&
+
 					part.contentType !== 'application/pkcs7-signature' &&
 					part.contentType !== 'application/x-pkcs7-signature' &&
 					(acc[mode] || (acc[mode] = [])).push(processAttachment(part, mode));
