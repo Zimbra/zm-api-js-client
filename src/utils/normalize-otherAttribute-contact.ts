7bb9ef3e66a169722e3767b0b94fd391651743a6
Damini Vashistha <damini.vashishtha@synacor.com>
2022-04-22 19:26:54 +0530

    ZBUG-2371 added phoneticCompany in contact attibutes

--- src/utils/normalize-otherAttribute-contact.ts
+++ src/utils/normalize-otherAttribute-contact.ts
@@ -7,6 +7,7 @@ import { ContactInputRequest } from '../normalize/entities';
 const supportedContactAttributes = [
 	'firstName',
 	'phoneticFirstName',
+	'phoneticCompany',
 	'middleName',
 	'lastName',
 	'phoneticLastName',
