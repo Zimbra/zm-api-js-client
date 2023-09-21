660217c203ace12f7738ac14a1f27da2bbf9dce6
Saket Patel <saket.patel@synacor.com>
2023-09-21 17:20:40 +0530

    87.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,7 @@ import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import graphql from 'rollup-plugin-graphql';
 import localResolve from 'rollup-plugin-local-resolve';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import { visualizer } from 'rollup-plugin-visualizer';
 import pkg from './package.json';
 
 let FORMAT = process.env.FORMAT;
@@ -11,13 +10,10 @@ let FORMAT = process.env.FORMAT;
 // graphql-tools currently has a rollup build failure, so always call it an external until they fix it
 // otherwise, make all npm production dependencies external, plus their subpath usages
 // throughout the codebase, which rollup doesn't automatically pick up on
-let external = FORMAT==='es' ?
-	Object.keys(pkg.dependencies)
-		.concat(
-			['castArray', 'get','isError', 'isObject', 'mapValues', 'reduce', 'omitBy', 'uniqBy', 'concat', 'uniqBy', 'differenceBy', 'forEach'].map(v => 'lodash/'+v),
-			['graphql']) :
-	[];
-
+let external = (Object.keys(pkg.dependencies) || [])
+	.concat(
+		['castArray', 'get','isError', 'isObject', 'mapValues', 'reduce', 'omitBy', 'uniqBy', 'concat', 'uniqBy', 'differenceBy', 'forEach'].map(v => 'lodash/'+v)
+	);
 
 export default {
 	external,
@@ -25,16 +21,13 @@ export default {
 	plugins: [
 		graphql(),
 		localResolve(),
-		nodeResolve({
-			extensions: [ '.js', '.ts', '.json' ]
-		}),
-		commonjs(),
 		typescript(),
 		babel({
 			extensions: ['.ts'],
 			exclude: 'node_modules/**',
 			babelHelpers: 'bundled'
-		})
+		}),
+		visualizer()
 	],
 	output: {
 		exports: FORMAT==='es' ? null : 'named'
