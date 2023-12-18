c4bf46691fc614a63ff5151f13cd916282b3d443
namita-uttarkar-telus <121433544+namita-uttarkar-telus@users.noreply.github.com>
2023-12-18 19:02:31 +0530

    PREAPPS-7326:  Disable Mail recall option if mail recall time is elapsed (#576)
    - Added zimraFeatureMailRecallEnabled and zimbraFeatureMailRecallTime LDAP attributes

--- src/schema/generated-schema-types.ts
+++ src/schema/generated-schema-types.ts
@@ -111,6 +111,8 @@ export type AccountInfoAttrs = {
   zimbraFeatureMailForwardingEnabled?: Maybe<Scalars['Boolean']>;
   zimbraFeatureMailForwardingInFiltersEnabled?: Maybe<Scalars['Boolean']>;
   zimbraFeatureMailPriorityEnabled?: Maybe<Scalars['Boolean']>;
+  zimbraFeatureMailRecallEnabled?: Maybe<Scalars['Boolean']>;
+  zimbraFeatureMailRecallTime?: Maybe<Scalars['Int']>;
   zimbraFeatureMailSendLaterEnabled?: Maybe<Scalars['Boolean']>;
   zimbraFeatureManageZimlets?: Maybe<Scalars['Boolean']>;
   zimbraFeatureMobileSyncEnabled?: Maybe<Scalars['Boolean']>;
