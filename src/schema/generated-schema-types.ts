93a0667ef31988aaa682307effac898b9766d844
Koichi Kato <koichi.kato@synacor.com>
2024-09-03 18:07:18 +0900

    4.40.2 PREAPPS-7988: fixed change password

--- src/schema/generated-schema-types.ts
+++ src/schema/generated-schema-types.ts
@@ -384,6 +384,7 @@ export type AuthResponse = {
   authToken?: Maybe<Array<Maybe<AuthToken>>>;
   csrfToken?: Maybe<CsrfToken>;
   lifetime?: Maybe<Scalars['Float']['output']>;
+  resetPassword?: Maybe<ResetPassword>;
   session?: Maybe<Session>;
   skin?: Maybe<Array<Maybe<Skin>>>;
   trustedDevicesEnabled?: Maybe<TrustedDevicesEnabled>;
@@ -2793,6 +2794,8 @@ export type MutationChangeFolderColorArgs = {
 
 
 export type MutationChangePasswordArgs = {
+  authToken?: InputMaybe<Scalars['String']['input']>;
+  csrfToken?: InputMaybe<Scalars['String']['input']>;
   dryRun?: InputMaybe<Scalars['Boolean']['input']>;
   loginNewPassword: Scalars['String']['input'];
   password: Scalars['String']['input'];
@@ -4034,6 +4037,11 @@ export type ReplyAction = {
 export type ReplyActionInput = {
   content?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
   index?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type ResetPassword = {
+  __typename?: 'ResetPassword';
+  _content?: Maybe<Scalars['Boolean']['output']>;
 };
 
 export type ResetPasswordResponse = {
